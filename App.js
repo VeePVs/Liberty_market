c7cb26ba901cbe8eebf61f8798f7d0a25e189d0f
Vee Pabuence Vargas <beepabuence@gmail.com>
2024-08-16 17:36:04 -0500

    Add dependencies StackNavigator and SafeAreaView

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {View} from "react-native";
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+
+//It's for managing windows in the app
+const Stack = createNativeStackNavigator();
 
 const App = () => (
-    <View>
 
-    </View>
 );
 
 export default App;
