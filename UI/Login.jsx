00b525c7d01c5d5707ee050dc0c293ee91843ed6
Vee Pabuence Vargas <beepabuence@gmail.com>
2024-08-16 21:30:25 -0500

    Added inputs and button to see password

--- UI/Login.jsx
+++ UI/Login.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import {StatusBar, Text, Sty} from 'react-native';
+import {Image, Pressable, StatusBar, Text, TextInput, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {styles} from '../styles/globalStyles'
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Login = () => {
     return (
         <SafeAreaView style={styles.container}>
             <StatusBar animated={true} statusBarStyle="dark-content" />
             <Text style={styles.title}>Inicio de sesión</Text>
+            <TextInput placeholder="Ingresa el usuario" style={styles.input} placeholderTextColor={"#000"}  maxLength={10}/>
+            <View style={styles.containerPassword}>
+                <TextInput placeholder="Ingresa la contraseña" style={styles.inputPassword} placeholderTextColor={"#000"} maxLength={8}/>
+                <Icon name="eye" size={30} style={styles.icon} onPress={()=>{
+
+                }}/>
+            </View>
+
         </SafeAreaView>
     );
 };
