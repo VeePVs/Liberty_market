d29421edbea4fa8b0296c4a9a5c37029d82f20f4
Vee Pabuence Vargas <beepabuence@gmail.com>
2024-08-17 22:37:25 -0500

    Created a PasswordInput component

---
+++ UI/Components/PasswordInput.jsx
@@ -0,0 +1,19 @@
+import { View, TextInput } from 'react-native'
+import React from 'react'
+import styles from '../../styles/globalStyles';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default function () {
+  const [visibleIcon, setVisibleIcon] = React.useState(false);
+  const [visiblePassword, setVisiblePassword] = React.useState(true);
+
+  return (
+    <View style={styles.containerPassword}>
+                <TextInput placeholder="Ingresa la contraseña" style={styles.inputPassword} placeholderTextColor={"#E1F7F5"} maxLength={8} secureTextEntry={visiblePassword}/>
+                <Icon name={visibleIcon== false ? 'eye-slash': 'eye'} size={30} style={styles.icon} onPress={()=>{
+                    setVisibleIcon(!visibleIcon);
+                    setVisiblePassword(!visiblePassword);
+                }}/>
+    </View>
+  )
+}
--- UI/Login.jsx
+++ UI/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Image, Pressable, StatusBar, Text, TextInput, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {styles} from '../styles/globalStyles'
-import Icon from 'react-native-vector-icons/FontAwesome';
+import PasswordInput from './Components/PasswordInput';
 
 const usuario = [{
     usuario: 'Vee',
@@ -11,21 +11,12 @@ const usuario = [{
 ]
 
 const Login = ({navigation}) => {
-    const [visible, setVisibleIcon] = React.useState(false);
-    const [visiblePassword, setVisiblePassword] = React.useState(true);
-
     return (
         <SafeAreaView style={styles.container}>
             <StatusBar animated={true} statusBarStyle="dark-content" />
             <Text style={styles.title}>Bienvenido a "nombre de la app"</Text>
             <TextInput placeholder="Ingresa el usuario" style={styles.input} placeholderTextColor={"#E1F7F5"}  maxLength={10}/>
-            <View style={styles.containerPassword}>
-                <TextInput placeholder="Ingresa la contraseña" style={styles.inputPassword} placeholderTextColor={"#E1F7F5"} maxLength={8} secureTextEntry={visiblePassword}/>
-                <Icon name={visible== false ? 'eye-slash': 'eye'} size={30} style={styles.icon} onPress={()=>{
-                    setVisibleIcon(!visible);
-                    setVisiblePassword(!visiblePassword);
-                }}/>
-            </View>
+            <PasswordInput />
             <Pressable style={styles.generalButton} onPress={()=>{
                 navigation.navigate('Register')
             }}>
@@ -34,7 +25,6 @@ const Login = ({navigation}) => {
             <Pressable style={styles.registerButton}>
                 <Text style={styles.textButton}>Continuar</Text>
             </Pressable>
-
         </SafeAreaView>
     );
 };
