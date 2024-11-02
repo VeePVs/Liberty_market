c70882b091fb6006c07eaaba5e1145b1a0e72215
Lea2132 <faberaristizabal41@gmail.com>
2024-11-02 06:04:16 -0500

    Favorites and Profile with firebase

--- UI/Favorites.jsx
+++ UI/Favorites.jsx
@@ -1,51 +1,29 @@
-import React from 'react';
-import { ScrollView } from 'react-native';
+import React, { useContext, useEffect, useState } from 'react';
+import { ScrollView} from 'react-native';
 import ProductItem from './Components/ProductItem';
 import styles from '../styles/FavoritesStyle';
-
-const products = [
-  {
-    id: '1',
-    image: 'https://cdn-4.nikon-cdn.com/e/Q5NM96RZZo-YRYNeYvAi9beHK4x3L-8u4h56I3YwHLAQ4G0XzTY4Dg==/Views/1590_D3500_right.png', 
-    description: 'Cámara DSLR Nikon D3500: Perfecta para principiantes con 24.2 MP y grabación Full HD.',
-    status: 'Disponible',
-  },
-  {
-    id: '2',
-    image: 'https://media.croma.com/image/upload/v1675347470/Croma%20Assets/Communication/Mobiles/Images/268868_u7nsk3.png', 
-    description: 'Smartphone Samsung Galaxy S23: Con pantalla AMOLED de 6.1" y procesador Exynos 2200.',
-    status: 'No disponible',
-  },
-  {
-    id: '3',
-    image: 'https://www.sony.com/is/image/gwtprod/a623e0ac346dea68e59c279e05221b59?fmt=png-alpha&wid=515&hei=515&trf=trim', 
-    description: 'Auriculares Sony WH-1000XM4: Cancelación de ruido avanzada y calidad de sonido premium.',
-    status: 'Disponible',
-  },
-  {
-    id: '4',
-    image: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP858/mbp16-gray.png', 
-    description: 'Laptop Apple MacBook Pro 16": Con procesador M1 Pro, ideal para profesionales y creativos.',
-    status: 'Disponible',
-  },
-  {
-    id: '5',
-    image: 'https://assets.mmsrg.com/isr/166325/c1/-/ASSET_MMS_86693664/fee_786_587_png', 
-    description: 'Smartwatch Fitbit Charge 5: Monitor de salud y actividad con GPS integrado y pantalla AMOLED.',
-    status: 'No disponible',
-  },
-  {
-    id: '6',
-    image: 'https://media.croma.com/image/upload/v1667851667/Croma%20Assets/Computers%20Peripherals/Tablets%20and%20iPads/Images/259175_12_hldr2e.png', 
-    description: 'Tablet Microsoft Surface Pro 8: Ideal para trabajo y entretenimiento con pantalla de 13" y Windows 11.',
-    status: 'Disponible',
-  },
-];
+import { UserContext } from './context/UserContext';
+import { getFavoriteProducts } from './database/firestore';
 
 const FavoritesScreen = () => {
+  const { userUID } = useContext(UserContext);
+  const [favoriteProducts, setFavoriteProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchFavorites = async () => {
+      try {
+        await getFavoriteProducts(userUID, setFavoriteProducts);
+      } catch (error) {
+        console.error("Error fetching favorite products:", error);
+      }
+    };
+
+    fetchFavorites();
+  }, [userUID,favoriteProducts]);
+
   return (
     <ScrollView style={styles.container}>
-      {products.map((product) => (
+      {favoriteProducts.map((product) => (
         <ProductItem key={product.id} product={product} />
       ))}
     </ScrollView>
